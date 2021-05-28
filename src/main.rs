5b9483cac1d03ad2a550f83383a48e475ea99dfa
Daan Lubbers <1859384+Daan4@users.noreply.github.com>
2021-05-29 01:55:16 +0200

    Initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use hvcv::components::{CircuitBreaker, Disconnector, EarthingSwitch, Component};
+
 fn main() {
-    println!("Hello, world!");
+    let cb = CircuitBreaker {};
+    let dis = Disconnector {};
+    let es = EarthingSwitch {};
+    println!("{:?} {:?} {:?}", Component::component_type(&cb), Component::component_type(&dis), Component::component_type(&es));
 }
