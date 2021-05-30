481a6c0eca33c549de9ed7577f37a46603b796ac
Daan Lubbers <1859384+Daan4@users.noreply.github.com>
2021-05-30 14:34:35 +0200

    Initial implementation and test

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,9 +1,9 @@
-use std::rc::Weak;
+use std::rc::Rc;
 
 use super::node::Node;
 
 pub struct Terminal {
-    node: Option<Weak<Node>>,
+    node: Option<Rc<Node>>,
 }
 
 impl Terminal {
@@ -13,11 +13,51 @@ impl Terminal {
         }
     }
 
-    pub fn connect() {
-
+    pub fn connect(&mut self, node: Rc<Node>) -> Result<(), &str> {
+        match self.node {
+            Some(_) => Err("Terminal already connected"),
+            None => {
+                self.node = Some(node.clone());
+                Ok(())
+            },
+        }
     }
 
-    pub fn get_node() {
+    pub fn disconnect(&mut self) -> Result<(), &str> {
+        match self.node {
+            Some(_) => {
+                self.node = None;
+                Ok(())
+            },
+            None => Err("Terminal not connected"),
+        }
+    }
 
+    pub fn get_node(&self) -> Result<Rc<Node>, &str> {
+        match self.node.clone() {
+            Some(node) => Ok(node),
+            None => Err("Terminal not connected"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn terminal_connect() {
+        let n = Rc::new(Node::new(None));
+        let mut t = Terminal::new();
+
+        assert!(t.disconnect().is_err());
+        assert!(t.get_node().is_err());
+
+        t.connect(n.clone()).unwrap();
+        assert!(Rc::ptr_eq(&t.get_node().unwrap(), &n));
+        assert!(t.connect(n).is_err());
+
+        t.disconnect().unwrap();
+        assert!(t.get_node().is_err());
     }
 }
